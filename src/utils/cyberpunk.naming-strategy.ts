590c9f800e24b2bd104b5738503a80c1f5c77678
Sonicfury <theo.lambrt@gmail.com>
2022-08-12 00:33:40 +0200

    chore: added annotation on cyberpunk naming strategy

--- src/utils/cyberpunk.naming-strategy.ts
+++ src/utils/cyberpunk.naming-strategy.ts
@@ -9,6 +9,12 @@ export class CyberpunkNamingStrategy implements INamingStrategy {
 
     name: string = 'cyberpunk';
 
+    /**
+     * Needs a resources list with at least one resource corresponding to a "gender" step, with "male" or "female" as name.
+     * @param config
+     * @param resources
+     * @param edition
+     */
     execute(config: any, resources: Resource[], edition: number): string {
         const genderValue: string | undefined = resources.find(r => r.step === 'gender')?.attribute?.value
 
